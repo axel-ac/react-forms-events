0f165c7db826b85776378b4a71759ed7232b9c81
axel-ac <abdullahceylan.axel@gmail.com>
2023-08-30 21:31:23 +0200

    mouse-events

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import Form from "./components/1-forms/Form"
-import FormWithObject from "./components/1-forms/FormWithObject copy"
-import KeyboardClipboard from "./components/2-keyboardClipboardEvents/KeyboardClipboard"
-
+import Form from "./components/1-forms/Form";
+import FormWithObject from "./components/1-forms/FormWithObject copy";
+import KeyboardClipboard from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
+import MouseEvent from "./components/3-mouseEvents/MouseEvent";
 
 function App() {
   return (
     <div className="container mt-4 text-center">
-      {/* <Form /> */}
-      {/* <FormWithObject /> */}
+      <Form />
+      <FormWithObject />
       <KeyboardClipboard />
+      <MouseEvent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
