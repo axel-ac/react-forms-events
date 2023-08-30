433e761c6feeb67044b0e75793109c5ff97971e5
axel-ac <abdullahceylan.axel@gmail.com>
2023-08-30 18:36:47 +0200

    keyboard-clipboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Form from "./components/1-forms/Form"
 import FormWithObject from "./components/1-forms/FormWithObject copy"
+import KeyboardClipboard from "./components/2-keyboardClipboardEvents/KeyboardClipboard"
 
 
 function App() {
   return (
     <div className="container mt-4 text-center">
       {/* <Form /> */}
-      <FormWithObject />
+      {/* <FormWithObject /> */}
+      <KeyboardClipboard />
     </div>
   )
 }
